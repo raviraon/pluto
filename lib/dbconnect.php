2e6cc3dc3358558efa82ed41c31ac5b767c3eb89
Ravi Rao N <raviraon@gmail.com>
2013-03-23 12:37:40 +0530

    Ravi | Adding mysql root password

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -5,7 +5,7 @@ class Database{
 	function __construct(){
 		$this->host = "127.0.0.1";
 		$this->username = "root"; 
-		$this->password  = ""; 
+		$this->password  = "9632"; 
 		$this->db_name   = "smactracker";
 		
 		$this->connect();
