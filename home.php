f8af1e2a24874f59197fbe3e86889a831033dff4
Ravi Rao N <raviraon@gmail.com>
2013-04-07 09:07:21 +0530

    Ravi C | Home page table with search, filtering, and other cool stuff. Thanks to DataTable plugin

--- home.php
+++ home.php
@@ -1,4 +1,7 @@
 <?php
+// ini_set('display_errors',1); 
+// error_reporting(E_ALL);
+
 session_start();
 require(__DIR__ . "/lib/check_user_logged_in.php");
 require(__DIR__ . "/lib/dbconnect.php");
@@ -11,25 +14,23 @@ $db->connect();
 $sql = "SELECT fusion_id, campaign, customer_name, open_date, close_date, status, created_at, updated_at FROM case_details WHERE user_id = $current_user_id ORDER BY updated_at DESC";
 $result = mysql_query($sql) or die(mysql_error());
 show();
-echo '	<div id="form_container">
-	<table>';
 while($row = mysql_fetch_object($result)){
-	echo "<tr> ";
+	echo ' <tr class="odd gradeA"> ';
 	foreach($row as $key => $value){
 		if($key == 'fusion_id'){		
 			echo "<td> <a href='view_case.php?case_id=$value'> $value </a> </td>";	
 		}
 		elseif( $key == 'open_date' || $key == 'closed_date'){
 			$formatted_date = format_date($value);
-			echo "<td> <label class='description'> $formatted_date  </label> </td>";				
+			echo "<td>  $formatted_date   </td>";				
 		}
 		else{
-			echo "<td> <label class='description'>$value </label> </td>";	
+			echo "<td> $value  </td>";	
 		}
 	}
 	echo "</tr>";
 }
-echo '</table></div></body>';
+echo '</tbody></table></div></body>';
 
 function show($errors='', $cases=null){
 	require('./html/home.html');
