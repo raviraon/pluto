624494dd135180db0dbf918e5e2cd13f71f573d1
Ravi Rao N <raviraon@gmail.com>
2013-04-14 14:25:18 +0530

    Ravi | Fixed sql files

--- sql/create_audit_case_owner_change_table.sql
+++ sql/create_audit_case_owner_change_table.sql
@@ -3,7 +3,8 @@ CREATE TABLE `audit_case_owner_change` (
 `fusion_id` varchar(255) NOT NULL default '',
 `old_case_owner` varchar(65) NOT NULL default '',
 `new_case_owner` varchar(65)  NULL default '',
-`updated_by` varchar(65)  NULL default '',
+`updated_by_name` varchar(65)  NULL default '',
+`updated_by_id` int(4) NOT NULL,
 `created_at` DATETIME NULL default NOW(),
 PRIMARY KEY (`id`)
 ) ENGINE=MyISAM;
--- sql/create_users.sql
+++ sql/create_users.sql
@@ -1,10 +1,8 @@
-CREATE TABLE `audit_case_owner_change` (
+CREATE TABLE `users` (
 `id` int(4) NOT NULL auto_increment,
-`fusion_id` varchar(255) NOT NULL default '',
-`old_case_owner` varchar(65) NOT NULL default '',
-`new_case_owner` varchar(65)  NULL default '',
-`updated_by_name` varchar(65)  NULL default '',
-`updated_by_id` int(4) NOT NULL,
-`created_at` DATETIME NULL default NOW(),
-PRIMARY KEY (`id`)
+`username` varchar(65) NOT NULL default '',
+`password` varchar(65) NOT NULL default '',
+`region` varchar(65)  NULL default '',
+PRIMARY KEY (`id`),
+UNIQUE (username)
 ) ENGINE=MyISAM;
